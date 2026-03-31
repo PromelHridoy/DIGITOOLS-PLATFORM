17c4a7198c9eb2d880de947b618bb4786db82258
PromelHridoy <promel.hridoy12@gmail.com>
2026-03-31 23:59:09 +0600

    I finished navber, banner, stats and steps

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+import Banner from './assets/component/Navbar/Banner/Banner'
+import Navbar from './assets/component/Navbar/Navbar'
+import Stats from './assets/component/Navbar/Stats/Stats'
+import Steps from './assets/component/Navbar/Steps/Steps'
+
+function App() {
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <Banner></Banner>
+      <Stats></Stats>
+      <Steps></Steps>
+    </>
+  )
+}
+
+export default App
