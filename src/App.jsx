46b6ff587e72bdbe25dc1315bd14b8c6cabc2d35
PromelHridoy <promel.hridoy12@gmail.com>
2026-04-01 00:44:39 +0600

    Added Pricing hover effects, CTA section, and Dark Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import Banner from './assets/component/Navbar/Banner/Banner'
 import Navbar from './assets/component/Navbar/Navbar'
 import Stats from './assets/component/Navbar/Stats/Stats'
 import Steps from './assets/component/Navbar/Steps/Steps'
+import Pricing from './assets/component/Pricing/Pricing'
+import CallToAction from './assets/component/CallToAction/CallToAction'
+import Footer from './assets/component/Footer/Footer'
+import ProductSection from './assets/component/ProductSection/ProductSection'
 
 function App() {
 
@@ -11,7 +15,11 @@ function App() {
       <Navbar></Navbar>
       <Banner></Banner>
       <Stats></Stats>
+      <ProductSection></ProductSection>
       <Steps></Steps>
+      <Pricing></Pricing>
+      <CallToAction></CallToAction>
+      <Footer></Footer>
     </>
   )
 }
